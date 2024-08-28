7d4ed0c045064dc28776b252d2b80734f7facceb
Revii28 <revirifaldi123@gmail.com>
2024-08-28 09:25:03 +0700

    feat: Create Migrations

---
+++ Product-Server/database/migrations/2024_08_28_022130_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name', 150)->notNull();
+            $table->string('category', 100)->notNull();
+            $table->decimal('price', 8, 2)->notNull();
+            $table->float('discount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
